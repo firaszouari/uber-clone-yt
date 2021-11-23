c04c53f5826c34c8d096f2d19c4b83b8d18bee37
Firas Zouari <firas.zouari@olivesoft.fr>
2021-11-23 12:46:09 +0100

    commit Uber-clone-app

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingViewBase, Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
+import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <KeyboardAvoidingViewBase 
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            style={{flex : 1}}
+            keyboardVerticalOffset={Platform.OS === "ios" ? -64 : 0}
+            >
+            <Stack.Navigator>
+              <Stack.Screen name="HomeScreen" component={HomeScreen} options={{
+                headerShown: false,
+              }} />
+              <Stack.Screen name="MapScreen" component={MapScreen} options={{
+                headerShown: false,
+              }} />
+            </Stack.Navigator>
+          </KeyboardAvoidingViewBase>
+        </SafeAreaProvider>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
---
+++ slices/navSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+    name: "nav",
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.travelTimeInformation = action.payload;
+        },
+    },
+});
+
+//Push information into the Data layer
+export const {setOrigin, setDestination, setTravelTimeInformation} = navSlice.actions;
+
+//Pull information from the Data layer using selector
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
